903dcee39c4e5f02a7cbfd89e0c4013320974825
shKlimenko <klimenko161@gmail.com>
2021-09-09 22:25:41 +0300

    проверка

--- src/main/java/ru/job4j/Tratata.java
+++ src/main/java/ru/job4j/Tratata.java
@@ -1,9 +1,7 @@
 package ru.job4j;
 
-public class Info {
+public class Tratata {
     public static void main(String[] args) {
-
-        System.out.println("09.09.2021");
-
+        System.out.println("GO GO GO");
     }
 }
