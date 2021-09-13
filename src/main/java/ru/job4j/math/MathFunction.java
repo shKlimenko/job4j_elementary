cfee81232dc1ad60716c65848edb6340757f6eae
shKlimenko <klimenko161@gmail.com>
2021-09-13 21:18:59 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -5,6 +5,7 @@ public class MathFunction {
     public static double sum(double first, double second) {
         return first + second;
     }
+
     public static double multiply(double first, double second) {
         return first * second;
     }
