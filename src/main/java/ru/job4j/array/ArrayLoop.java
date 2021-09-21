cf617bca6148ab5d41afffed9f95d400795d58f8
PK rabota <klimenko161@gmail.com>
2021-09-21 16:09:13 +0300

    Массивы и цикл for-each c исправлениями

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,8 +6,8 @@ public class ArrayLoop {
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int chtoUgodno : numbers) {
-            System.out.println(chtoUgodno);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
