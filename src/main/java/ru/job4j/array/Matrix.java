286ca512c3640189d5db21a3283451ef2dcc061a
PK rabota <klimenko161@gmail.com>
2021-09-23 14:36:51 +0300

    Исправлено согласно замечанию (таблица умножения)

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] tabl = new int[size][size];
-        for (int i = 0; i < tabl.length; i++) {
-            for (int j = 0; j < tabl[i].length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 tabl[i][j] = (i + 1) * (j + 1);
             }
         }
