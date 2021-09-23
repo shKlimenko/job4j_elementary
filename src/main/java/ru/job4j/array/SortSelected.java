2e7c98e3926ef6b523c2b439e4a450a5317f2943
PK rabota <klimenko161@gmail.com>
2021-09-23 14:22:54 +0300

    Исправлено согласно замечанию

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,7 +4,7 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
             data = SwitchArray.swap(data, i, index);
         }
         return data;
