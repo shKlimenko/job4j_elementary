c5d9f289934be250adf8a0f137e8b397cf0784d9
PK rabota <klimenko161@gmail.com>
2021-09-25 09:12:34 +0300

    Жадный алгоритм. Доработка.

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int cash = money - price;
-        for (int i = 0; i < 4; i++) {
-            while (cash >= coins[i]) {
-                cash -= coins[i];
-                rsl[size] = coins[i];
+        for (int coin : coins) {
+            while (cash >= coin) {
+                cash -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
