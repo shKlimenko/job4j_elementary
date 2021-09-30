d52f009b1e4d7707e111d8b0a8505bbf50cb3ea6
shKlimenko <klimenko161@gmail.com>
2021-09-30 21:13:23 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,8 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-
         System.out.println("09.09.2025");
-
     }
 }
--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
 public class X2 {
-
     public static int calc(int a, int b, int c, int x) {
         return a * x * x + b * x + c;
     }
